ec147bc1e259b85021ab804073c737bf179251b0
giloup933 <gilp93@gmail.com>
2020-05-13 05:34:36 -0500

    knighttour works!

--- knighttour.pl
+++ knighttour.pl
@@ -1,16 +1,25 @@
-moves(X,Y,M) :- X1 is X+1, XM1 is X-1, X2 is X+2, XM2 is X-2,
-    Y1 is Y+1, YM1 is Y-1, Y2 is Y+2, YM2 is Y-2,
-    append([[X2,Y1],[X2,YM1],[X1,Y2],[X1,YM2]],[[XM1,Y2],[XM1,YM2],[XM2,Y1],[XM2,YM1]], M).
-filter([X,Y]) :- X>0, Y>0, X<9, Y<9.
-hasbeen(P,[X,Y]) :- member([X,Y],P).
-legalmoves(X,Y,P,M) :- moves(X,Y,M1), include(filter,M1,M2), exclude(hasbeen(P),M2,M).
+move(X0,Y0,X,Y) :- 
+    X is X0+2, Y is Y0+1;
+    X is X0+2, Y is Y0-1;
+    X is X0+1, Y is Y0+2;
+    X is X0+1, Y is Y0-2;
+    X is X0-1, Y is Y0+2;
+    X is X0-1, Y is Y0-2;
+    X is X0-2, Y is Y0+1;
+    X is X0-2, Y is Y0-1.
+hasbeen(P,X,Y) :- member([X,Y],P).
+legalmove(N,X0,Y0,P,[X,Y]) :- move(X0,Y0,X,Y), X>0, Y>0, X=<N, Y=<N, not(member([X,Y],P)).
+heuristic(N,X0,Y0,P,H) :- 
+    findall([X,Y],legalmove(N,X0,Y0,P,[X,Y]),Bag), length(Bag,H).
+mvh(N,X0,Y0,P,[H,X,Y]) :- legalmove(N,X0,Y0,P,[X,Y]), heuristic(N,X,Y,P,H).    
+warnsdorff(N,X0,Y0,P,M) :- 
+    findall([X,Y,H],mvh(N,X0,Y0,P,[X,Y,H]),Bag),
+    sort(1,@=<,Bag,SBag),
+    member(M,SBag).
+tour(N,X0,Y0,P,T) :- append(P,[[X0,Y0]],T), L is N*N, length(T,L).
+tour(N,X0,Y0,P,T) :- append(P,[[X0,Y0]],P1), L is N*N, not(length(P1,L)), 
+    warnsdorff(N,X0,Y0,P,[_,X,Y]), tour(N,X,Y,P1,T).
+knighttour(N,X,Y,T) :- tour(N,X,Y,[],T).
 
-tourlevel([_,_], _, [], []).
-tourlevel([X,Y],P,T,[HD|_]) :- append(P,[X,Y],P1), tour(HD,P1,T), length(T,64).
-tourlevel([X,Y],P,T,[HD|TL]) :- append(P,[X,Y],P1), tour(HD,P1,T), not(length(T,64)), tourlevel([X,Y],P,T,TL).
 
-tour([X,Y],P,[]) :- legalmoves(X,Y,P,M), M=[], not(length(P,64)).
-tour([X,Y],P,P) :- legalmoves(X,Y,P,M), M=[], length(P,64).
-tour([X,Y],P,T) :- legalmoves(X,Y,P,M), not(M=[]), tourlevel([X,Y],P,T,M).
-                                                                                       
-knighttour(X,Y,T) :- tour([X,Y],[[X,Y]],T).
+% knighttour(24,1,1,T);
